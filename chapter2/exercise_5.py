9778eaafd7323b21dfd18266fcfbba034b39e3f6
Eric Schles <ericschles@gmail.com>
2014-03-23 18:13:37 -0700

    exercise 5

---
+++ chapter2/exercise_5.py
@@ -0,0 +1,11 @@
+"""
+Results:
+x = input()
+3.14 entered
+result of print type(x): <type 'float'>
+x = raw_input()
+3.14 entered
+result of print type(x): <type 'str'>
+x = input()
+'we are the knights who say "ni!"'
+result of print type(x): <type 'str'>
