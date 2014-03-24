ff801b2db040c50d61978b094cc8bd14450b30d5
Eric Schles <ericschles@gmail.com>
2014-03-23 17:37:47 -0700

    exercise 1 fixed

--- chapter1/exercise_1.py
+++ chapter1/exercise_1.py
@@ -12,14 +12,14 @@ I smiled into the cat.
 # Below is a simple program that writes syntactically inaccurate sentences.
 
 from random import choice
-nouns = ["Eric","cat","dog","snowflake","snowman","cheese","Noam"]
-verbs = ["Ran","walked","ran","running","playing","jumping","laughing"]
+nouns = ["Eric ","cat ","dog ","snowflake ","snowman ","cheese" ,"Noam "]
+verbs = ["Ran ","walked ","ran ","running ","playing ","jumping ","laughing "]
 
 sentence = ""
 for i in xrange(15):
     if i%2 == 0:
-        sentence += choice(noun)
+        sentence += choice(nouns)
     else:
-        setence += choice(verb)
+        sentence += choice(verbs)
 
 print sentence+"."
