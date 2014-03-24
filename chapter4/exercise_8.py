74f50a55144366417947d729473b1df9153d1321
Eric Schles <ericschles@gmail.com>
2014-03-24 07:42:57 -0700

    exercise 8 done

---
+++ chapter4/exercise_8.py
@@ -0,0 +1,13 @@
+"""
+if "Ni!":
+    print 'We are the Knights who say, "Ni!"'
+else:
+    print "Stop it! No more of this!"
+
+if 0:
+    print "And now for something completely different..."
+else:
+    print "What's all this, then?"
+
+The above code will always print 'We are the Knights who say, "Ni!"', because "Ni!" evaluates to true since the string is non-empty.
+"""
