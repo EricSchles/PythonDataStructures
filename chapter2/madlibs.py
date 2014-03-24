9b4c7f397e7545f75a88d3439bb7dbbab981c8b7
Eric Schles <ericschles@gmail.com>
2014-03-23 18:24:35 -0700

    madlibs done

---
+++ chapter2/madlibs.py
@@ -0,0 +1,9 @@
+noun_one = str(raw_input("Enter a noun:"))
+noun_two = str(raw_input("Enter another noun:"))
+verb_one = str(raw_input("Enter a verb:"))
+verb_two = str(raw_input("Enter a second verb:"))
+past_tense_verb = str(raw_input("Enter a past tense verb:"))
+
+sentence = noun_one+ " saw " + noun_two + " by the pool and shouted!  Then " + noun_one +" "+ verb_one + " over to "+noun_two+" he was very pleased with this.  Then " +noun_two+" realized that the sky was falling and thought it might have been "+noun_one+"'s fault.  "+noun_two +" wailed and wailed, why did you "+past_tense_verb+" you've killed us all.  But sadly it wasn't "+noun_one+"'s fault.  No it was actually your fault.  For not recycling ofcourse!"
+
+print sentence
