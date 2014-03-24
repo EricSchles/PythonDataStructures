ccd74296ba20175a8bcbd7a929d5412035c8a399
Eric Schles <ericschles@gmail.com>
2014-03-23 17:43:00 -0700

    exercise 3 done

---
+++ chapter1/exercise_3.py
@@ -0,0 +1,13 @@
+"""
+The following code doesn't work, on purpose
+1)
+1 2 
+-- SyntaxError: invalid syntax
+2)
+"h" + 1
+--TypeError: cannot concatenate "str" and "int" objects
+3)
+thing + 1
+--NameErro name 'thing' is not defined
+"""
+
