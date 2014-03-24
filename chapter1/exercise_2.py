fba8e6dcd7773555212c91187ed1c0197f9b882f
Eric Schles <ericschles@gmail.com>
2014-03-23 17:40:01 -0700

    exercise 2 done

---
+++ chapter1/exercise_2.py
@@ -0,0 +1,4 @@
+print 1 + 2
+"""
+If you use the / operator you do integer division, assuming you have two ints.  If one of your operands is a double then it will do floating point division aka "normal division"
+""" 
