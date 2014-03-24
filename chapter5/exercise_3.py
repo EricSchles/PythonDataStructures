41c6bea68a848b41c333837b63699bec1ed6d17f
Eric Schles <ericschles@gmail.com>
2014-03-24 10:17:12 -0700

    didn't read end of problem 3 done now

--- chapter5/exercise_3.py
+++ chapter5/exercise_3.py
@@ -21,6 +21,20 @@ def slope(x1,y1,x2,y2):
         return y_delta/x_delta
     return "invalid argument specified, must be numbers!!"
 
+def intercept(x1,y1,x2,y2):
+    """
+    >>> intercept(1,6,3,12)
+    3.0
+    >>> intercept(6,1,1,6)
+    7.0
+    >>> intercept(4,6,12,8)
+    5.0
+    """
+    if insist_number([x1,y1,x2,y2]):
+        alpha = slope(x1,y1,x2,y2)
+        beta = y2 - (alpha*x2)
+        return beta
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
