747075234013fefac08b07f5374b33c46819c464
ericschles <ericschles@gmail.com>
2014-04-17 08:33:17 -0400

    exercise 2 added

---
+++ chapter6/exercise_2.py
@@ -0,0 +1,10 @@
+def sqrt(n):
+    approx = n/2.0
+    better = (approx + n/approx)/2.0
+    while better != approx:
+        approx = better
+        better = (approx + n/approx)/2.0
+        print better
+    return approx
+
+print sqrt(25)
